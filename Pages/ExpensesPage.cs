122ca63f0b2ad92a6986a836df882c9e7fda39ec
Cubex332 <167002605+Cubex332@users.noreply.github.com>
2026-03-27 23:51:03 +0500

    UPD

--- Pages/ExpensesPage.cs
+++ Pages/ExpensesPage.cs
@@ -8,14 +8,19 @@ namespace SpendNote.Pages
         public ExpensesPage(IScreenshotProtectionService screenshotProtection) {
             NavigationPage.SetHasNavigationBar(this, false);
             var mainLayout = new Grid();
+            
             var createExpenses = new Button
             {
                 Text = "+",
                 VerticalOptions = LayoutOptions.End,
                 HorizontalOptions = LayoutOptions.End,
                 Margin = 20,
-                BorderWidth=20
+                CornerRadius = 30,
+                WidthRequest = 60,
+                HeightRequest = 60,
+                FontSize = 30
             };
+            
             _screenService = screenshotProtection;
             mainLayout.Children.Add(createExpenses);
             Content = mainLayout;
@@ -25,6 +30,11 @@ namespace SpendNote.Pages
         {
             base.OnAppearing();
             _screenService.Disable();
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            return true;
         }
     }
 }
